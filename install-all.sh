e2ac47894752b1aed75213c2ccdc2e51769fb9a6
J Freeman <155204947+jmf-pobox@users.noreply.github.com>
2026-03-10 01:26:13 -0700

    chore: update vox install SHA to v1.6.0 (#69)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- install-all.sh
+++ install-all.sh
@@ -57,7 +57,7 @@ banner "quarry"
 curl -fsSL "$GH/quarry/ab576f0/install.sh" | sh
 
 banner "vox"
-curl -fsSL "$GH/vox/e919bdb/install.sh" | sh
+curl -fsSL "$GH/vox/7a94962/install.sh" | sh
 
 banner "lux"
 curl -fsSL "$GH/lux/bd953f6/install.sh" | sh
